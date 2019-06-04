7e308a468a7f5846e54f7477e663cdecd08e4a4c
Simo Siren <simo.siren@stud.pointcollege.fi>
2019-06-04 16:41:33 +0300

    Sarakkeiden lajittelutoiminto tehty ryhmiin, ryhmäluokkiin ja kategorialuokkiin.

--- CRM-Demo/CRM-Demo/App_Start/BundleConfig.cs
+++ CRM-Demo/CRM-Demo/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace CRM_Demo
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
